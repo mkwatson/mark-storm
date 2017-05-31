255f48737c9426acceee759133c7075ae734056f
Boaz Rosenan <brosenan@gmail.com>
2017-05-31 08:35:40 +0300

    adding a unit test + acks

--- ch2/test/ch2/core_test.clj
+++ ch2/test/ch2/core_test.clj
@@ -1,7 +1,16 @@
 (ns ch2.core-test
   (:require [clojure.test :refer :all]
-            [ch2.core :refer :all]))
+            [ch2.core :refer :all]
+            [org.apache.storm.testing :as st]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-topology
+  (st/with-simulated-time-local-cluster [cluster]
+    (let [result (st/complete-topology cluster
+                                       (github-commit-count-topology)
+                                       :mock-sources
+                                       {"1" [["aaaa foo@bar.com"]
+                                             ["bbbb bar@bar.com"]
+                                             ["cccc foo@bar.com"]]})]
+      (testing "Email Extractor"
+        (is (st/ms= [["foo@bar.com"] ["foo@bar.com"] ["bar@bar.com"]]
+                    (st/read-tuples result "2")))))))
